b57cf1c2d25ef04c58eb25c405de9f7695987a4d
mashkoukras ( reza as ) <55289441+mashkouk@users.noreply.github.com>
2021-04-27 15:56:02 +0430

    Update and rename arrow_surya.mk to zeus_surya.mk

--- zeus_surya.mk
+++ zeus_surya.mk
@@ -10,18 +10,17 @@ $(call inherit-product, device/xiaomi/surya/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/zeus/config/common.mk)
 
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_surya
+PRODUCT_NAME := zeus_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X3 NFC
 PRODUCT_MANUFACTURER := Xiaomi
-DEVICE_MAINTAINER := Ganesh Varma
 
 BUILD_FINGERPRINT := POCO/surya_global/surya:10/QKQ1.200512.002/V12.0.3.0.QJGMIXM:user/release-keys
 TARGET_INCLUDE_PIXEL_CHARGER := true
